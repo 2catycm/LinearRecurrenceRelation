53f4292cb3ccd0d40c6aa806e54d5442aa066349
叶璨铭 <12011404@mail.sustech.edu.cn>
2022-01-16 00:45:33 +0800

    fix(LinearAlgebraSolver.java): svd is for AAT, and U is not V, so VTU != I. now fixed to eigen values and eigen vectors trivially.

--- src/main/java/sample/FibonacciNumber.java
+++ src/main/java/sample/FibonacciNumber.java
@@ -1,14 +1,15 @@
 package sample;
 
 import org.apache.commons.math3.analysis.UnivariateFunction;
+import src.HomogeneousLinearRecurrenceRelation;
 import src.LinearAlgebraSolver;
 
 public class FibonacciNumber {
     public static void main(String[] args) {
-        final var linearAlgebraSolver = new LinearAlgebraSolver(new double[]{1, 1}, new double[]{0, 1});
-        final var generalTermFormula = linearAlgebraSolver.getGeneralTermFormula();
+        final HomogeneousLinearRecurrenceRelation solver = new LinearAlgebraSolver(new double[]{1, 1}, new double[]{1, 0});
+        final var generalTermFormula = solver.getGeneralTermFormula();
         for (int i = 0; i < 100; i++) {
-            System.out.println(generalTermFormula.value(i));
+            System.out.println((long)generalTermFormula.value(i));
         }
     }
 }
--- src/main/java/src/LinearAlgebraSolver.java
+++ src/main/java/src/LinearAlgebraSolver.java
@@ -16,30 +16,35 @@ public class LinearAlgebraSolver extends HomogeneousLinearRecurrenceRelation {
      */
     @Override
     public UnivariateFunction getGeneralTermFormula() {
-        final var doubles = new double[order][order];
-        System.arraycopy(coefficients, 0, doubles[0], 0, order); //copy to avoid possible change by matrix operations.
-        for (int i = 1; i < doubles.length; i++) {
-            doubles[i][i - 1] = 1;
+        final var valuesMatrixA = new double[order][order];
+        System.arraycopy(coefficients, 0, valuesMatrixA[0], 0, order); //copy to avoid possible change by matrix operations.
+        for (int i = 1; i < valuesMatrixA.length; i++) {
+            valuesMatrixA[i][i - 1] = 1;
         }
-        final var matrixA = new Array2DRowRealMatrix(doubles, false);
-        final var svd = new SingularValueDecomposition(matrixA);
-        final var matrixU = svd.getU();
-        final var matrixVT = svd.getVT();
-        final var matrixLambda = new DiagonalMatrix(svd.getSingularValues());
-        if (debugMode) {
-            final var I = MatrixUtils.createRealIdentityMatrix(order);
-            if (!matrixVT.multiply(matrixU).equals(I))
-                throw new AssertionError("SVD wrong.");
+        final var matrixA = new Array2DRowRealMatrix(valuesMatrixA, false);
+        final var ed = new EigenDecomposition(matrixA);
+        final var eigenvalues = ed.getRealEigenvalues();
+        if (debugMode && eigenvalues.length!=order)
+            throw new AssertionError("real eigen values not enough. ");
+        final var matrixLambda = new DiagonalMatrix(eigenvalues);
+
+        final var valuesMatrixS = new double[order][];
+        for (int i = 0; i < order; i++) {
+            valuesMatrixS[i] = ed.getEigenvector(i).toArray();
         }
-        return new UnivariateFunction() {
-            @Override
-            public double value(double n) {
-                if (debugMode && Math.abs(n - (int) n) > 1e-5)
-                    throw new AssertionError("general term formula only computes sequence.");
-                final var APowerN = matrixU.multiply(matrixLambda.power(order)).multiply(matrixVT);
-                RealVector vectorN = APowerN.operate(initialVector);
-                return vectorN.getEntry(order-1);
-            }
+        final var matrixS = new Array2DRowRealMatrix(valuesMatrixS).transpose();
+        final var matrixSI = MatrixUtils.inverse(matrixS);
+//        if (debugMode){
+//            final var I = MatrixUtils.createRealIdentityMatrix(order);
+//            if (!matrixS.multiply(matrixSI).equals(I))
+//                throw new AssertionError("not accurate.");
+//        }
+        return n -> {
+            if (debugMode && Math.abs(n - (int) n) > 1e-5)
+                throw new AssertionError("general term formula only computes sequence.");
+            final var APowerN = matrixS.multiply(matrixLambda.power((int)n)).multiply(matrixSI);
+            RealVector vectorN = APowerN.operate(initialVector);
+            return vectorN.getEntry(order-1);
         };
     }
 }
